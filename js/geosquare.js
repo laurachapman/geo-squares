394f45c424a3497157949c9115779620142d885e
LAURA CHAPMAN <lchapman@college.harvard.edu>
2020-02-20 15:47:20 -0500

    random triangle happiness

--- js/geosquare.js
+++ js/geosquare.js
@@ -6,8 +6,15 @@
  * @param _data						-- the  
  */
 
-GeoSquare = function(_parentElement, color_1, color_2, color_3, color_4,
-                     numrowcol_a, chaos_1, chaos_2, size){
+GeoSquare = function(_parentElement,
+                     color_1, color_2, color_3, color_4,
+                     numrowcol_a,
+                     chaos_1, chaos_2,
+                     size,
+                     numlevels, numinlevels,
+                     triangle_bools = [true, true, true, true],
+                     triangle_values = [2, 4, 7, 5]){
+
 	this.parentElement = _parentElement;
 	this.color_1 = color_1;
 	this.color_2 = color_2;
@@ -15,9 +22,12 @@ GeoSquare = function(_parentElement, color_1, color_2, color_3, color_4,
 	this.color_4 = color_4;
 
 	this.size = size;
-	console.log("this.size:", this.size)
 
-	this.num_levels = 2;
+	this.triangle_bools = triangle_bools;
+	this.triangle_values = triangle_values;
+
+	this.num_levels = numlevels;
+	this.num_in_levels = numinlevels;
 
 	this.num_row = numrowcol_a;
 	this.num_col = numrowcol_a;
@@ -58,22 +68,21 @@ GeoSquare.prototype.initVis = function(){
 	    .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
 
 	// draw the shapes
-	vis.updateShapes(vis.color_1, vis.color_2, vis.color_3,
-        vis.num_row, vis.num_col, vis.chaos_1, vis.chaos_2);
+	vis.updateShapes(vis.color_1, vis.color_2, vis.color_3, vis.color_4,
+        vis.num_row, vis.num_col, vis.chaos_1, vis.chaos_2, vis.num_levels, vis.num_in_levels,
+        vis.triangle_bools, vis.triangle_values);
 }
 
 
-/*
- * The drawing function - should use the D3 update sequence (enter, update, exit)
- * Function parameters only needed if different kinds of updates are needed
- */
-
 GeoSquare.prototype.updateShapes = function(newcolor1, newcolor2, newcolor3, newcolor4,
-                                            numrow, numcol, newchaos_1, newchaos_2){
+                                            numrow, numcol, newchaos_1, newchaos_2, numlevels,
+                                            numinlevels, tbools, tvals){
 	var vis = this;
 
 	// remove all previous squares
 	vis.svg.selectAll(".design-squares").remove();
+	// remove all previous triangles
+    vis.svg.selectAll(".triangles").remove();
 
 	// update the parameters
 	vis.color_1 = newcolor1;
@@ -83,51 +92,183 @@ GeoSquare.prototype.updateShapes = function(newcolor1, newcolor2, newcolor3, new
 	vis.num_col = numcol;
 	vis.chaos_1 = newchaos_1;
 	vis.chaos_2 = newchaos_2;
+	vis.num_levels = numlevels;
+	vis.num_in_levels = numinlevels;
+	vis.triangle_bools = tbools;
+	vis.triangle_values = tvals;
 
-	console.log("num col:", vis.num_col, "num row:", vis.num_row);
+    // console.log("vis:", vis);
 
-    vis.squarewidth = vis.width / vis.num_col;
-	vis.squareheight = vis.height / vis.num_row;
+    var square_width = vis.width / vis.num_col;
+	var square_height = vis.height / vis.num_row;
 
 	var count = 0;
 
 	//if num_row is even then we have a lineup problem
 
 	// iterate over rows and columns
-    for (var c=0; c<vis.num_col; c++){
-        for (var r=0; r<vis.num_row; r++){
-            var xpos = c*vis.squarewidth;
-            var ypos = r*vis.squareheight;
+    for (var c=0; c<vis.num_col; c=c+1){
+        for (var r=0; r<vis.num_row; r=r+1){
+            var xpos = c * square_width;
+            var ypos = r * square_height;
 
             //pass in level parameters
-            vis.drawSquare(xpos, ypos, count);
+            // console.log("about to call drawSquare with level:", vis.num_levels);
+
+            vis.drawSquare(xpos, ypos, square_width, square_height,
+                count, vis.num_levels);
 
             count = count + 1;
         }
     }
 }
 
-GeoSquare.prototype.drawSquare = function(x, y, count){
+GeoSquare.prototype.drawSquare = function(x, y, w, h, count, level){
     var vis = this;
 
-    vis.svg.append("rect")
-        .attr("height", vis.squareheight)
-        .attr("width", vis.squarewidth)
-        .attr("x", x)
-        .attr("y", y)
-        .attr("class", "design-squares")
-        .attr("fill", function(){
-            if (count % vis.chaos_1 === 0){
-                return vis.color_3;
+    // console.log("drawsquare called with level:", level);
+    // console.log("vis.num_in_levels:", vis.num_in_levels);
+    // console.log("vis:", vis);
+
+    // if we are at the bottom level, go ahead and draw the square
+    // ACCORDING to the rules
+    if (level === 0){
+        // console.log("here, level was 0");
+
+        vis.svg.append("rect")
+            .attr("height", w)
+            .attr("width", h)
+            .attr("x", x)
+            .attr("y", y)
+            .attr("class", "design-squares")
+            .attr("fill", function(){
+                if (count % vis.chaos_1 === 0){
+                    return vis.color_3;
+                }
+                else if (count % vis.chaos_2 === 0){
+                    return vis.color_4;
+                }
+                else if (count % 2 === 0){
+                    return vis.color_1;
+                }
+                else{
+                    return vis.color_2;
+                }
+            });
+
+    //    append triangles if relevant
+        for (var i=0; i<4; i=i+1){
+            if (vis.triangle_bools[i]){
+                vis.drawTriangle(i, x, y, w, h, count);
             }
-            else if (count % vis.chaos_2 === 0){
-                return vis.color_4;
+        }
+
+    }
+
+    else{
+        var new_width = w / vis.num_in_levels[level];
+        var new_height = h / vis.num_in_levels[level];
+    //    iterate through inner rows and columns and draw a square for each
+        var count_again = 0;
+
+        // console.log("vis.num_in_levels:", vis.num_in_levels, "level:", level,
+        //     "vis.num_in_levels[level]:", vis.num_in_levels[level-1])
+
+        for (var c=0; c<4; c=c+1){
+            // console.log("in first for")
+            for (var r=0; r<4; r=r+1){
+                // console.log("in second for")
+                var xpos = x + (c * new_width);
+                var ypos = y + (r * new_height);
+
+                //pass in level parameters
+                vis.drawSquare(xpos, ypos, new_width, new_height,
+                    count_again, level-1);
+
+                count_again = count_again + 1;
             }
-            else if (count % 2 === 0){
-                return vis.color_1;
-            }
-            else{
-                return vis.color_2;
-            }
-        });
+        }
+    }
+
+}
+
+
+GeoSquare.prototype.drawTriangle = function(num, x, y, w, h, count){
+    var vis = this;
+
+    //    append a triangle each way
+    var mypath = "";
+    var mycolor = "";
+
+    if (num===0){
+        mypath = "M " + x + " " + y +
+            " L " + x + " " + (y+h) +
+            " L " + (x+h) + " " + (y+h) +
+            " L " + x + " " + y;
+        mycolor = vis.color_1;
+    }
+    else if (num===1){
+        mypath = "M " + x + " " + y +
+            " L " + (x+h) + " " + y +
+            " L " + (x+h) + " " + (y+h) +
+            " L " + x + " " + y;
+        mycolor = vis.color_2;
+    }
+    else if (num===2){
+        mypath = "M " + x + " " + y +
+            " L " + (x+h) + " " + y +
+            " L " + (x+h) + " " + (y+h) +
+            " L " + x + " " + y;
+        mycolor = vis.color_3;
+    }
+    else{
+        mypath = "M " + (x+h) + " " + y +
+            " L " + x + " " + y +
+            " L " + x + " " + (y+h) +
+            " L " + (x+h) + " " + y
+        mycolor = vis.color_4
+    }
+
+    if (count % vis.triangle_values[num] === 0){
+        vis.svg.append("path").attr("d", mypath)
+            .attr("class", "triangles")
+            .attr("fill", mycolor);
+    }
+
+    // if (count % 3 == 0){
+    //     vis.svg.append("path")
+    //         .attr("d",
+    //             "M " + x + " " + y +
+    //             " L " + x + " " + (y+h) +
+    //             " L " + (x+h) + " " + (y+h) +
+    //             " L " + x + " " + y
+    //         )
+    //         .attr("fill", vis.color_4)
+    //
+    // }
+    //
+    // //
+    // if (count % 3 == 1){
+    //     vis.svg.append("path")
+    //         .attr("d",
+    //             "M " + x + " " + y +
+    //             " L " + (x+h) + " " + y +
+    //             " L " + (x+h) + " " + (y+h) +
+    //             " L " + x + " " + y
+    //         )
+    //         .attr("fill", vis.color_3)
+    //
+    // }
+    // //    append a triangle every 3
+    // if (count % 3 == 2){
+    //     vis.svg.append("path")
+    //         .attr("d",
+    //             "M " + (x+h) + " " + y +
+    //             " L " + (x+h) + " " + (y+h) +
+    //             " L " + x + " " + (y+h) +
+    //             " L " + (x+h) + " " + y
+    //         )
+    //         .attr("fill", vis.color_1)
+    //
+    // }
 }
--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
 
 //
 var geosquares = [];
+// var elements = [];
+var numsquares = 15;
 
 var masterparent = document.getElementById("square-parent");
 
-for (var i=1; i<2; i=i+1){
+for (var i=1; i<(numsquares + 1); i=i+1){
 	var newsquare = document.createElement("div");
 	newsquare.setAttribute("id", "square-" + i);
+
 	masterparent.appendChild(newsquare);
 
-	geosquares.push(createVis("square-" + i));
+	var newgeo = createVis("square-" + i);
+	// newsquare.setAttribute("onclick", "refreshOneVis(newgeo)")
+
+
+	geosquares.push(newgeo);
+	// elements.push(newsquare);
 }
 
 function createVis(parent) {
@@ -19,12 +27,29 @@ function createVis(parent) {
 	var color_3 = randColor();
 	var color_4 = randColor();
 
-	var numrowcol_a = randBetween(3,50);
-	var chaos_1 = randBetween(3, 10);
-	var chaos_2 = randBetween(5,15);
+	var numrowcol_a = randBetween(3,15);
+	var chaos_1 = randBetween(3, 8);
+	var chaos_2 = randBetween(3,8);
+
+	// var numlevels = randBetween(0, 3);
+	var numlevels = 0
+	var numinlevels = [];
+
+	//make the triangle arrays
+	var tbools = [];
+	var tvals = [];
+	for (var i=0; i<4; i=i+1){
+		tbools.push(randBool());
+		tvals.push(randBetween(2, 10));
+	}
+
+	for (var i=0; i<numlevels; i=i+1){
+		numinlevels.push(randBetween(3,8));
+	}
 
 	return new GeoSquare(parent, color_1, color_2, color_3, color_4,
-		numrowcol_a, chaos_1, chaos_2, 500);
+		numrowcol_a, chaos_1, chaos_2, 200, numlevels, numinlevels,
+		tbools, tvals);
 }
 
 //refreshes all
@@ -41,20 +66,44 @@ function refreshOneVis(square) {
     var newcolor_3 = randColor();
     var newcolor_4 = randColor();
 
-    var newnum_a = randBetween(3,50);
-    var newnum_b = randBetween(3,50);
+    var newnum_a = randBetween(3,15);
+    var newnum_b = randBetween(3,15);
 
-    var newchaos_1 = randBetween(3, 10);
-    var newchaos_2 = randBetween(5,15);
+    var newchaos_1 = randBetween(3, 8);
+    var newchaos_2 = randBetween(3,8);
+
+    // var numlevels = randBetween(0,3);
+	var numlevels = 0;
+    var numinlevels = [];
+
+    for (var i=0; i<numlevels; i=i+1){
+        numinlevels.push(randBetween(3,8));
+    }
+    //make the triangle arrays
+    var tbools = [];
+    var tvals = [];
+    for (var i=0; i<4; i=i+1){
+        tbools.push(randBool());
+        tvals.push(randBetween(2, 10));
+    }
+
+    for (var i=0; i<numlevels; i=i+1) {
+        numinlevels.push(randBetween(3, 8));
+    }
 
 	square.updateShapes(newcolor_1, newcolor_2, newcolor_3, newcolor_4,
 		newnum_a, newnum_a,
-		newchaos_1, newchaos_2);
+		newchaos_1, newchaos_2, numlevels, numinlevels,
+		tbools, tvals);
 }
 
 function randBetween(x, y) {
 	var diff = y-x;
 	return (Math.floor(Math.random() * diff) + x)
+}
+
+function randBool(){
+	return (Math.random() < 0.6);
 }
 
 function randColor(){
