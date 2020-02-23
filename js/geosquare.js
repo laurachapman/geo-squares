123ea4f5b70ed41abbdd20193e1f06fe020a1d85
LAURA CHAPMAN <lchapman@college.harvard.edu>
2020-02-23 13:19:45 -0500

    fixed so there are no more little lines around the shapes

--- js/geosquare.js
+++ js/geosquare.js
@@ -6,38 +6,60 @@
  * @param _data						-- the  
  */
 
-GeoSquare = function(_parentElement,
-                     color_1, color_2, color_3, color_4,
-                     numrowcol_a,
-                     chaos_1, chaos_2,
-                     size,
-                     numlevels, numinlevels,
-                     triangle_bools = [true, true, true, true],
-                     triangle_values = [2, 4, 7, 5]){
+GeoSquare = function(_parentElement, size, config){
 
 	this.parentElement = _parentElement;
-	this.color_1 = color_1;
-	this.color_2 = color_2;
-	this.color_3 = color_3;
-	this.color_4 = color_4;
-
 	this.size = size;
+	this.config = config;
 
-	this.triangle_bools = triangle_bools;
-	this.triangle_values = triangle_values;
+    this.num_row = config["newnum_a"];
+    this.num_col = config["newnum_a"];
 
-	this.num_levels = numlevels;
-	this.num_in_levels = numinlevels;
+    this.multilevel = config["multilevel"];
+    this.multinum = config["multinum"];
 
-	this.num_row = numrowcol_a;
-	this.num_col = numrowcol_a;
-	this.chaos_1 = chaos_1;
-	this.chaos_2 = chaos_2;
-
-    this.initVis();
+	this.initVis();
 }
 
 
+GeoSquare.prototype.setConfigs = function(config){
+    var vis = this;
+
+    vis.color_1 = config["color_1"];
+    vis.color_2 = config["color_2"];
+    vis.color_3 = config["color_3"];
+    vis.color_4 = config["color_4"];
+    vis.color_5 = config["color_5"];
+
+    vis.triangle_bools = config["tbools"];
+    vis.triangle_values = config["tvals"];
+
+    vis.num_levels = config["numlevels"];
+    vis.num_in_levels = config["num_in_levels"];
+
+    vis.num_row = config["newnum_a"];
+    vis.num_col = config["newnum_a"];
+    vis.chaos_1 = config["chaos_1"];
+    vis.chaos_2 = config["chaos_2"];
+
+    vis.circle_spacing = config["circle_spacing"];
+    vis.circle_frac = config["circle_frac"];
+
+    vis.cboolmid = config["cboolmid"];
+    vis.cboolintersect = config["cboolintersect"];
+    vis.circle_schemes = config["circle_schemes"];
+
+    vis.little_tbools = config["little_tbools"];
+    vis.little_tvals = config["little_tvals"];
+
+    vis.multilevel = config["multilevel"];
+    vis.multinum = config["multinum"];
+
+    vis.trans = config["translucency"];
+
+    vis.config = config;
+
+}
 
 /*
  * Initialize visualization (static content, e.g. SVG area or axes)
@@ -46,89 +68,116 @@ GeoSquare = function(_parentElement,
 GeoSquare.prototype.initVis = function(){
 	var vis = this;
 
-	var margins_all = vis.size * 0.02
-	vis.margin = { top: margins_all, right: margins_all, bottom: margins_all, left: margins_all };
-
-	vis.width = this.size - vis.margin.left - vis.margin.right,
-    vis.height = this.size - vis.margin.top - vis.margin.bottom;
-
-
-  // SVG drawing area
-	vis.svg = d3.select("#" + vis.parentElement).append("svg")
-	    .attr("width", vis.width + vis.margin.left + vis.margin.right)
-	    .attr("height", vis.height + vis.margin.top + vis.margin.bottom);
-
-	// add black border
-    vis.svg.append("rect")
-        .attr("width", vis.width + vis.margin.left + vis.margin.right)
-        .attr("height", vis.height + vis.margin.top + vis.margin.left)
-        .attr("fill", "black");
-
-	vis.svg = vis.svg.append("g")
-	    .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
+    // SVG drawing area
+    vis.svg_canvas = d3.select("#" + vis.parentElement).append("svg")
+        .attr("id", "mysvg")
+        .attr("width", vis.size)
+        .attr("height", vis.size);
 
 	// draw the shapes
-	vis.updateShapes(vis.color_1, vis.color_2, vis.color_3, vis.color_4,
-        vis.num_row, vis.num_col, vis.chaos_1, vis.chaos_2, vis.num_levels, vis.num_in_levels,
-        vis.triangle_bools, vis.triangle_values);
+	vis.updateShapes(vis.config);
 }
 
 
-GeoSquare.prototype.updateShapes = function(newcolor1, newcolor2, newcolor3, newcolor4,
-                                            numrow, numcol, newchaos_1, newchaos_2, numlevels,
-                                            numinlevels, tbools, tvals){
+GeoSquare.prototype.updateShapes = function(config){
 	var vis = this;
+	console.log("in updateshapes")
 
 	// remove all previous squares
-	vis.svg.selectAll(".design-squares").remove();
+	vis.svg_canvas.selectAll(".design-squares").remove();
 	// remove all previous triangles
-    vis.svg.selectAll(".triangles").remove();
+    vis.svg_canvas.selectAll(".triangles").remove();
+    //remove all previous circles
+    vis.svg_canvas.selectAll(".circles").remove();
+    // remove previous border
+    vis.svg_canvas.selectAll(".border").remove();
 
-	// update the parameters
-	vis.color_1 = newcolor1;
-	vis.color_2 = newcolor2;
-	vis.color_3 = newcolor3;
-	vis.num_row = numrow;
-	vis.num_col = numcol;
-	vis.chaos_1 = newchaos_1;
-	vis.chaos_2 = newchaos_2;
-	vis.num_levels = numlevels;
-	vis.num_in_levels = numinlevels;
-	vis.triangle_bools = tbools;
-	vis.triangle_values = tvals;
+    // remove the previous svg
+    // d3.select("#mysvg").remove();
 
-    // console.log("vis:", vis);
+    // update the parameters
+    vis.setConfigs(config);
 
-    var square_width = vis.width / vis.num_col;
-	var square_height = vis.height / vis.num_row;
+    var margins_all = vis.size * 0.02;
 
-	var count = 0;
+    if (vis.multinum) {
+        var design_space = (vis.multinum * vis.num_row)*(Math.floor(vis.size / (vis.multinum * vis.num_row)));
+        margins_all = (vis.size - design_space) * 0.5;
+    }
+    else{
+        var design_space = vis.num_row * (Math.floor(vis.size / vis.num_row));
+        margins_all = (vis.size-design_space) * 0.5;
+    }
 
-	//if num_row is even then we have a lineup problem
+    vis.margin = { top: margins_all, right: margins_all, bottom: margins_all, left: margins_all };
 
-	// iterate over rows and columns
-    for (var c=0; c<vis.num_col; c=c+1){
-        for (var r=0; r<vis.num_row; r=r+1){
-            var xpos = c * square_width;
-            var ypos = r * square_height;
+    vis.width = vis.size - vis.margin.left - vis.margin.right,
+        vis.height = vis.size - vis.margin.top - vis.margin.bottom;
 
-            //pass in level parameters
-            // console.log("about to call drawSquare with level:", vis.num_levels);
+    // add colored border
+    vis.svg_canvas.append("rect")
+        .attr("width", vis.width + vis.margin.left + vis.margin.right)
+        .attr("height", vis.height + vis.margin.top + vis.margin.left)
+        .attr("class", "border")
+        .attr("fill", vis.color_1);
 
-            vis.drawSquare(xpos, ypos, square_width, square_height,
-                count, vis.num_levels);
+    vis.svg = vis.svg_canvas.append("g")
+        .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
 
-            count = count + 1;
+	// if vis.multilevel, then do all of this for each megarow megacolumn (default 3)
+    if (vis.multilevel){
+        var square_width = Math.floor(vis.width / (vis.multinum * vis.num_col));
+        var square_height = Math.floor(vis.width / (vis.multinum * vis.num_row));
+
+        var bigsquare_width = square_width * vis.num_col;
+        var bigsquare_height = square_width * vis.num_row;
+
+        for (var bc=0; bc<vis.multinum; bc = bc+1){
+            for (var br=0; br<vis.multinum; br = br+1){
+
+                var count = 0;
+
+                // iterate over rows and columns
+                for (var c=0; c<vis.num_col; c=c+1){
+                    for (var r=0; r<vis.num_row; r=r+1){
+                        var xpos = c * square_width;
+                        var ypos = r * square_height;
+
+                        vis.drawSquare(xpos + (bc * bigsquare_height),
+                            ypos + (br * bigsquare_width), square_width, square_height,
+                            count, vis.num_levels, c, r);
+
+                        count = count + 1;
+                    }
+                }
+            }
+        }
+    }
+
+    else{
+        var square_width = Math.floor(vis.width / vis.num_col);
+        var square_height = Math.floor(vis.height / vis.num_row);
+
+        var count = 0;
+
+
+        // iterate over rows and columns
+        for (var c=0; c<vis.num_col; c=c+1){
+            for (var r=0; r<vis.num_row; r=r+1){
+                var xpos = c * square_width;
+                var ypos = r * square_height;
+
+                vis.drawSquare(xpos, ypos, square_width, square_height,
+                    count, vis.num_levels, c, r);
+
+                count = count + 1;
+            }
         }
     }
 }
 
-GeoSquare.prototype.drawSquare = function(x, y, w, h, count, level){
+GeoSquare.prototype.drawSquare = function(x, y, w, h, count, level, c, r){
     var vis = this;
-
-    // console.log("drawsquare called with level:", level);
-    // console.log("vis.num_in_levels:", vis.num_in_levels);
-    // console.log("vis:", vis);
 
     // if we are at the bottom level, go ahead and draw the square
     // ACCORDING to the rules
@@ -163,6 +212,46 @@ GeoSquare.prototype.drawSquare = function(x, y, w, h, count, level){
             }
         }
 
+        for (var i=0; i<4; i=i+1){
+            if (vis.little_tbools[i]){
+                vis.drawLittleTriangle(i, x, y, w, h, count);
+            }
+        }
+
+        // these circles look shitty
+        // if(vis.cboolmid) {
+        //     //    append circle if count mod spacing is 2
+        //     if (count % vis.circle_spacing === 2){
+        //         vis.drawMidCircle(x, y, w/2, count);
+        //     }
+        // }
+
+        // console.log("c:", c, "r:", r, "circle_spacing:", vis.circle_spacing);
+        if(vis.cboolintersect) {
+            if (vis.circle_schemes[0]){
+                if ((c*r-r) % vis.circle_spacing === 0){
+                    if (c > 0 && r > 0){
+                        vis.drawXCircle(x, y, w/2, vis.color_3);
+                    }
+                }
+            }
+            if (vis.circle_schemes[1]){
+                if (c+r % vis.circle_spacing === 0){
+                    if (c > 0 && r > 0){
+                        vis.drawXCircle(x, y, w/2, vis.color_5);
+                    }
+                }
+            }
+            if (vis.circle_schemes[2]) {
+                if (count % vis.circle_spacing === 0){
+                    if (c > 0 && r > 0){
+                        vis.drawXCircle(x, y, w/2, vis.color_4);
+                    }
+                }
+            }
+        }
+
+
     }
 
     else{
@@ -193,6 +282,92 @@ GeoSquare.prototype.drawSquare = function(x, y, w, h, count, level){
 }
 
 
+GeoSquare.prototype.drawLittleTriangle = function(num, x, y, w, h, count){
+    var vis = this;
+
+    //    append a triangle each way
+    var mypath = "";
+    var mycolor = "";
+
+    // four different orientations of triangles
+    if (num===0){
+        //top
+        mypath = "M " + x + " " + y +
+            " L " + (x+(h/2)) + " " + (y+(h/2)) +
+            " L " + (x+h) + " " + y +
+            " L " + x + " " + y;
+        mycolor = vis.color_5;
+    }
+    else if (num===1){
+        // left
+        mypath = "M " + x + " " + y +
+            " L " + (x+(h/2)) + " " + (y+(h/2)) +
+            " L " + (x+h) + " " + y +
+            " L " + x + " " + y;
+        mycolor = vis.color_3;
+    }
+    else if (num===2){
+        //bottom
+        mypath = "M " + x + " " + (y+h) +
+            " L " + (x+(h/2)) + " " + (y+(h/2)) +
+            " L " + (x+h) + " " + (y+h) +
+            " L " + x + " " + (y+h);
+        mycolor = vis.color_1;
+    }
+    else{
+        //right
+        mypath = "M " + (x+h) + " " + y +
+            " L " + (x+h) + " " + (y+h) +
+            " L " + (x+(h/2)) + " " + (y+(h/2)) +
+            " L " + (x+h) + " " + y
+        mycolor = vis.color_4;
+    }
+
+    if (count % vis.little_tvals[num] === 0){
+        vis.svg.append("path").attr("d", mypath)
+            .attr("class", "triangles")
+            .attr("fill", mycolor)
+            .attr("opacity", vis.trans);
+    }
+
+}
+
+// x and y should be the top left corner where the circle is to be drawn
+// r is the radius (half width of surrounding square)
+GeoSquare.prototype.drawXCircle = function(x, y, r, color){
+    var vis = this;
+
+    vis.svg.append("circle")
+        .attr("r", r * vis.circle_frac)
+        .attr("cx", x)
+        .attr("cy", y)
+        .attr("class", "circles")
+        .attr("fill", color)
+        .attr("opacity", vis.trans);
+}
+
+// x and y should be the top left corner where the circle is to be drawn
+// r is the radius (half width of surrounding square)
+GeoSquare.prototype.drawMidCircle = function(x, y, r, count){
+    var vis = this;
+
+    vis.svg.append("circle")
+        .attr("r", r * vis.circle_frac)
+        .attr("cx", x+r)
+        .attr("cy", y+r)
+        .attr("class", "circles")
+        .attr("fill", function(count){
+            //make this more sophistocated and randomized
+            if (count % 3 === 0){
+                return vis.color_4;
+            }
+            else{
+                return vis.color_5;
+            }
+        });
+}
+
+
 GeoSquare.prototype.drawTriangle = function(num, x, y, w, h, count){
     var vis = this;
 
@@ -200,6 +375,7 @@ GeoSquare.prototype.drawTriangle = function(num, x, y, w, h, count){
     var mypath = "";
     var mycolor = "";
 
+    // four different orientations of triangles
     if (num===0){
         mypath = "M " + x + " " + y +
             " L " + x + " " + (y+h) +
@@ -212,7 +388,7 @@ GeoSquare.prototype.drawTriangle = function(num, x, y, w, h, count){
             " L " + (x+h) + " " + y +
             " L " + (x+h) + " " + (y+h) +
             " L " + x + " " + y;
-        mycolor = vis.color_2;
+        mycolor = vis.color_5;
     }
     else if (num===2){
         mypath = "M " + x + " " + y +
@@ -232,43 +408,7 @@ GeoSquare.prototype.drawTriangle = function(num, x, y, w, h, count){
     if (count % vis.triangle_values[num] === 0){
         vis.svg.append("path").attr("d", mypath)
             .attr("class", "triangles")
-            .attr("fill", mycolor);
+            .attr("fill", mycolor)
+            .attr("opacity", vis.trans);
     }
-
-    // if (count % 3 == 0){
-    //     vis.svg.append("path")
-    //         .attr("d",
-    //             "M " + x + " " + y +
-    //             " L " + x + " " + (y+h) +
-    //             " L " + (x+h) + " " + (y+h) +
-    //             " L " + x + " " + y
-    //         )
-    //         .attr("fill", vis.color_4)
-    //
-    // }
-    //
-    // //
-    // if (count % 3 == 1){
-    //     vis.svg.append("path")
-    //         .attr("d",
-    //             "M " + x + " " + y +
-    //             " L " + (x+h) + " " + y +
-    //             " L " + (x+h) + " " + (y+h) +
-    //             " L " + x + " " + y
-    //         )
-    //         .attr("fill", vis.color_3)
-    //
-    // }
-    // //    append a triangle every 3
-    // if (count % 3 == 2){
-    //     vis.svg.append("path")
-    //         .attr("d",
-    //             "M " + (x+h) + " " + y +
-    //             " L " + (x+h) + " " + (y+h) +
-    //             " L " + x + " " + (y+h) +
-    //             " L " + (x+h) + " " + y
-    //         )
-    //         .attr("fill", vis.color_1)
-    //
-    // }
 }
--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 var geosquares = [];
 // var elements = [];
 var numsquares = 15;
+var squaresize = 200;
 
 var masterparent = document.getElementById("square-parent");
 
@@ -15,41 +16,14 @@ for (var i=1; i<(numsquares + 1); i=i+1){
 	var newgeo = createVis("square-" + i);
 	// newsquare.setAttribute("onclick", "refreshOneVis(newgeo)")
 
-
 	geosquares.push(newgeo);
 	// elements.push(newsquare);
 }
 
 function createVis(parent) {
 	// generate parameters- numbers colors etc
-	var color_1 = randColor();
-	var color_2 = randColor();
-	var color_3 = randColor();
-	var color_4 = randColor();
-
-	var numrowcol_a = randBetween(3,15);
-	var chaos_1 = randBetween(3, 8);
-	var chaos_2 = randBetween(3,8);
-
-	// var numlevels = randBetween(0, 3);
-	var numlevels = 0
-	var numinlevels = [];
-
-	//make the triangle arrays
-	var tbools = [];
-	var tvals = [];
-	for (var i=0; i<4; i=i+1){
-		tbools.push(randBool());
-		tvals.push(randBetween(2, 10));
-	}
-
-	for (var i=0; i<numlevels; i=i+1){
-		numinlevels.push(randBetween(3,8));
-	}
-
-	return new GeoSquare(parent, color_1, color_2, color_3, color_4,
-		numrowcol_a, chaos_1, chaos_2, 200, numlevels, numinlevels,
-		tbools, tvals);
+	var config = generateConfig();
+	return new GeoSquare(parent, squaresize, config);
 }
 
 //refreshes all
@@ -61,19 +35,52 @@ function refreshVis() {
 
 //gets a geosquare object as input
 function refreshOneVis(square) {
+    var config = generateConfig();
+	square.updateShapes(config);
+}
+
+function randBetween(x, y) {
+	var diff = y-x;
+	return (Math.floor(Math.random() * diff) + x)
+}
+
+function randBool(){
+	return (Math.random() < 0.5);
+}
+
+function randColor(){
+	return '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+}
+
+function generateConfig(){
     var newcolor_1 = randColor();
     var newcolor_2 = randColor();
     var newcolor_3 = randColor();
-    var newcolor_4 = randColor();
 
-    var newnum_a = randBetween(3,15);
-    var newnum_b = randBetween(3,15);
+    var newcolor_4 = "";
+    var newcolor_5 = "";
+
+    if (randBool()){
+    	newcolor_5 = randColor();
+	}
+	else{
+		newcolor_5 = newcolor_3;
+	}
+	if (randBool()){
+		newcolor_4 = randColor();
+	}
+	else{
+		newcolor_4 = newcolor_2;
+	}
+
+    var newnum_a = randBetween(3,20);
+    // var newnum_b = randBetween(3,15);
 
     var newchaos_1 = randBetween(3, 8);
     var newchaos_2 = randBetween(3,8);
 
     // var numlevels = randBetween(0,3);
-	var numlevels = 0;
+    var numlevels = 0;
     var numinlevels = [];
 
     for (var i=0; i<numlevels; i=i+1){
@@ -87,25 +94,72 @@ function refreshOneVis(square) {
         tvals.push(randBetween(2, 10));
     }
 
+    //make the triangle arrays
+    var ltbools = [];
+    var ltvals = [];
+    for (var i=0; i<4; i=i+1){
+        ltbools.push(randBool());
+        ltvals.push(randBetween(2, 10));
+    }
+
+
     for (var i=0; i<numlevels; i=i+1) {
         numinlevels.push(randBetween(3, 8));
     }
 
-	square.updateShapes(newcolor_1, newcolor_2, newcolor_3, newcolor_4,
-		newnum_a, newnum_a,
-		newchaos_1, newchaos_2, numlevels, numinlevels,
-		tbools, tvals);
-}
+    var circle_spacing = randBetween(2, 8);
+    var circle_frac = (Math.random()/2)+0.5;
+    var cboolmid = randBool();
+    var cboolintersect = randBool();
+    // var circle_scheme = randBetween(1, 4);
 
-function randBetween(x, y) {
-	var diff = y-x;
-	return (Math.floor(Math.random() * diff) + x)
-}
+	var circle_schemes = []
+	for (var i=0; i<4; i=i+1){
+		circle_schemes.push(randBool());
+	}
 
-function randBool(){
-	return (Math.random() < 0.6);
-}
+	var multilevel = true;
+	if (newnum_a > 10){
+		multilevel = false;
+	}
+	else if (newnum_a < 5){
+		multilevel = true;
+	}
+	else {
+		multilevel = randBool();
+	}
 
-function randColor(){
-	return '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+	var multinum = randBetween(3,6);
+
+	var transluce = 1;
+	if (randBool()){
+		transluce = (Math.random()/2)+ 0.25;
+	}
+
+    var config = {"color_1": newcolor_1,
+	"color_2": newcolor_2,
+	"color_3": newcolor_3,
+	"color_4": newcolor_4,
+		"color_5": newcolor_5,
+	"newnum_a": newnum_a,
+	"chaos_1": newchaos_1,
+		"chaos_2": newchaos_2,
+		"numlevels": numlevels,
+		"numinlevels": numinlevels,
+		"tbools": tbools,
+		"tvals": tvals,
+		"circle_spacing": circle_spacing,
+		"circle_frac": circle_frac,
+		"cboolmid": cboolmid,
+		"cboolintersect": cboolintersect,
+		"little_tvals": ltvals,
+		"little_tbools": ltbools,
+		"circle_schemes": circle_schemes,
+		"multilevel": multilevel,
+		"multinum": multinum,
+		"translucency": transluce
+
+	}
+
+	return config;
 }
